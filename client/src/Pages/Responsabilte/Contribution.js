5e7bbe3e48698dbad8f7a3925df01d202397e0a8
Khawla-bk <benkadidakhawla@gmail.com>
2022-03-01 13:01:30 +0100

    responsabilites

--- client/src/Pages/Responsabilte/Contribution.js
+++ client/src/Pages/Responsabilte/Contribution.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Responsabilte.css";
 
-const Contribution = ({el}) => {
+const Contribution = ({ el }) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div>
-      <img src={el.img} alt="img" />
-      <h4>{el.title} </h4>
-      <p>{el.desc1}</p>
-      <p>{el.desc2}</p>
+    <div className="grid-layout">
+      <div className="visisble">
+        <h4 className="odd-title">{el.title} </h4>
+        <img src={el.img} alt="img" />
+        <p className="grid-description">{el.desc1}</p>
       </div>
+      <div>
+        {show ? (
+          <p id="hidden" className="grid-description">
+            {el.desc2}
+          </p>
+        ) : null}
+        <div className="btn-container">
+          <button id="btnA" onClick={() => setShow(true)}>
+            Afficher
+          </button>
+          <button id="btnM" onClick={() => setShow(false)}>
+            Masquer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Contribution
+export default Contribution;
--- client/src/Pages/Responsabilte/Responsabilte.js
+++ client/src/Pages/Responsabilte/Responsabilte.js
@@ -2,11 +2,21 @@ import React from "react";
 import HeaderBackground from "../../Components/HeaderBackground/HeaderBackground";
 import MainLayout from "../../MainLayout/MainLayout";
 import Contribution from "./Contribution";
+import "./Responsabilte.css";
+import volet from "./volet.png";
+import valeur from "./valeur politique.png";
+import a from "./1.png";
+import b from "./2.png";
+import c from "./3.png";
+import d from "./4.png";
+import e from "./5.png";
+import f from "./6.png";
+import g from "./7.png";
 
 const Responsabilte = () => {
   const contribution = [
     {
-      img: "",
+      img: a,
       title: "Compétences et accès à l’emploi ",
       desc1:
         "4.4 : D’ici à 2030, augmenter considérablement le nombre de jeunes et d’adultes disposant des compétences, notamment techniques et professionnelles, nécessaires à l’emploi, à l’obtention d’un travail décent et à l’entrepreneuriat",
@@ -14,7 +24,7 @@ const Responsabilte = () => {
         "En 2021 et malgré la crise sanitaire,  la STB SICAR, qui compte 12 collaborateurs, a assuré la formation de 4 stagiaires aux métiers du capital investissement.Également et en application de sa politique d’ouverture sur l’université, la STB SICAR et dans le cadre du partenariat avec l’école supérieure des sciences et technologies du design, un challenge pour la rénovation de son siège social a été lancé et a concerné 40 étudiants.",
     },
     {
-      img: "",
+      img: b,
       title: "Lutte contre les discriminations ",
       desc1:
         "5.1 : Mettre fin, dans le monde entier, à toutes les formes de discrimination à l’égard des femmes et des filles.Participation et accès aux postes de direction 5.5 : Garantir la participation entière et effective des femmes et leur accès en toute égalité aux fonctions de direction à tous les niveaux de décision, dans la vie politique, économique et publique.",
@@ -22,7 +32,7 @@ const Responsabilte = () => {
         "L’effectif de STB SICAR est composé de 70% de femmes contre 30% d’hommes.La femme est représentée à hauteur de 20% dans son conseil d’administration.STB SICAR suit également la politique mise en œuvre auprès des entreprises qu’elle accompagne en ce qui concerne l’emploi des femmes et leur participation aux postes de responsabilité.",
     },
     {
-      img: "",
+      img: c,
       title: "Énergies renouvelables",
       desc1:
         "7.2 : D’ici à 2030, accroître nettement la part de l’énergie renouvelable dans le bouquet énergétique mondial",
@@ -30,7 +40,7 @@ const Responsabilte = () => {
         "En 2020, sur 7 projets financés 2 ont concerné des projets industriels d’énergies renouvelables. ",
     },
     {
-      img: "",
+      img: d,
       title: "Développement des TPE et PME",
       desc1:
         "8.3 : Promouvoir des politiques axées sur le développement qui favorisent des activités productives, la création d’emplois décents, l’entrepreneuriat, la créativité et l’innovation et stimulent la croissance des micro-entreprises et des petites et moyennes entreprises et facilitent leur intégration dans le secteur formel, y compris par l’accès aux services financiers",
@@ -38,7 +48,7 @@ const Responsabilte = () => {
         "Entre 2016 et 2020, la STB SICAR a contribué à la création voire maintien de plus de 1700 postes.Entre 2018 et 2020, plus de 20 projets de création et e développement de PME ont été approuvés.Pour la période 2016-2020, et si l’on se réfère au calcul du levier (Coût total des projets investis/Montant  investi), un dinar investi par la STB SICAR en draine en moyenne 8 en termes d’investissement.",
     },
     {
-      img: "",
+      img: e,
       title: "Industrialisation socio-économiquement durable",
       desc1:
         "9.2 : Promouvoir une industrialisation durable qui profite à tous et, d’ici à 2030, augmenter nettement la contribution de l’industrie à l’emploi et au produit intérieur brut, en fonction du contexte national, et la multiplier par deux dans les pays les moins avancés",
@@ -46,7 +56,7 @@ const Responsabilte = () => {
         "La STB SICAR a réalisé 297 investissements répartis sur tout le territoire dont 53% dans l’industrie. ",
     },
     {
-      img: "",
+      img: f,
       title: "Réduction des déchets",
       desc1:
         "12.5 : D’ici à 2030, réduire considérablement la production de déchets par la prévention, la réduction, le recyclage et la réutilisation",
@@ -54,7 +64,7 @@ const Responsabilte = () => {
         "La STB SICAR a fait un don du matériel informatique usagé à l’association « sharek » au profit d’étudiants tunisiens qui en ont besoin afin de poursuivre les programmes à distance.La STB SICAR a signé une convention de partenariat avec El Moraskal pour le recyclage du papier ",
     },
     {
-      img: "",
+      img: g,
       title: "Institutions exemplaires",
       desc1:
         "16.6 : Mettre en place des institutions efficaces, responsables et transparentes à tous les niveaux.Prise de décisions inclusive:16.7 : Faire en sorte que le dynamisme, l’ouverture, la participation et la représentation à tous les niveaux caractérisent la prise de décisions",
@@ -64,45 +74,53 @@ const Responsabilte = () => {
   ];
   return (
     <>
-    <MainLayout>
-      <HeaderBackground/>
-    <div className="responsabilte">
-      <div className="politique">
-        <h4>Notre politique interne </h4>
-        <p>
-          Nous permettons à nos collaborateurs de grandir au sein de STB SICAR,
-          aussi bien professionnellement que personnellement. Nous veillons à
-          l’égalité entre tous et à l’accomplissement de chacun.
-        </p>
-        <h6>En Bref</h6>
-        <div className="en__bref__img"></div>
-      </div>
-      <div className="pour_nos_participations">
-        <p>
-          Nous sommes conscients de la nécessité de prendre en considération les
-          critères environnementaux, sociaux et de qualité de gouvernance (ESG)
-          dans la politique d’investissement. Ainsi, nous avons établi une
-          charte ESG. Nous sommes également très attachés à la qualité de
-          l’approche et à la politique mise en œuvre par nos participations en
-          ce qui concerne le respect de l’environnement et la gestion des
-          ressources humaines. Pour ce faire, un questionnaire a été envoyé aux
-          sociétés financées. Il s’appuie sur un socle commun comprenant des
-          indicateurs quantitatifs portant sur les 3 grands thèmes ESG
-          prioritaires de la STB SICAR.
-        </p>
-        <div className="pour_nos_participations__img"></div>
-        <p>Ce questionnaire sera envoyé annuellement.</p>
-      </div>
-      <div className="contribution_Odd">
-        <h5>
-          La STB SICAR contribue aux objectifs de développement durable
-          proportionnellement aux moyens dont elle dispose. Au regard de ses
-          activités, elle contribue principalement aux 7 ODD suivants :
-        </h5>
-        {contribution.map(el=><Contribution  el={el}/>)}
-      </div>
-    </div>
-    </MainLayout>
+      <MainLayout>
+        <HeaderBackground />
+        <h1 className="page-title">Responsabilité Sociale</h1>
+        <div className="responsabilte">
+          <div className="politique">
+            <h3>Notre politique interne </h3>
+            <p className="page-description">
+              Nous permettons à nos collaborateurs de grandir au sein de STB
+              SICAR, aussi bien professionnellement que personnellement. Nous
+              veillons à l’égalité entre tous et à l’accomplissement de chacun.
+            </p>
+            <img src={valeur} alt="" width="100%" className="valeurImage" />
+          </div>
+
+          <div className="pour_nos_participations">
+            <h3>Pour Notre Participation</h3>
+            <p className="page-description">
+              Nous sommes conscients de la nécessité de prendre en considération
+              les critères environnementaux, sociaux et de qualité de
+              gouvernance (ESG) dans la politique d’investissement. Ainsi, nous
+              avons établi une charte ESG. Nous sommes également très attachés à
+              la qualité de l’approche et à la politique mise en œuvre par nos
+              participations en ce qui concerne le respect de l’environnement et
+              la gestion des ressources humaines. Pour ce faire, un
+              questionnaire a été envoyé aux sociétés financées. Il s’appuie sur
+              un socle commun comprenant des indicateurs quantitatifs portant
+              sur les 3 grands thèmes ESG prioritaires de la STB SICAR.
+            </p>
+            <div className="voletDiv">
+              <img src={volet} alt="" width="50%" />
+            </div>
+          </div>
+          <div className="contribution_Odd">
+            <h3>Contribution ODD</h3>
+            <p className="page-description">
+              La STB SICAR contribue aux objectifs de développement durable
+              proportionnellement aux moyens dont elle dispose. Au regard de ses
+              activités, elle contribue principalement aux 7 ODD suivants :
+            </p>
+            <div className="odd-main-container">
+              {contribution.map((el) => (
+                <Contribution el={el} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </MainLayout>
     </>
   );
 };
