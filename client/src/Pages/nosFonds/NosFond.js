c5ed91ba42afd317d51b1db773d5c228e57a8ce5
attia mouadh <attiamou3adh@gamil.com>
2022-03-01 13:23:44 +0100

    nosFonds

--- client/src/Pages/nosFonds/NosFond.js
+++ client/src/Pages/nosFonds/NosFond.js
@@ -1,70 +1,146 @@
-import React from 'react'
-import nosFond_img1 from "./img/nosFond_img1.png"
-import nosFond_img2 from "./img/nosFond-img2.png"
-import nosFond_img3 from "./img/nosFond-img3.png"
-import nosFond_img4 from "./img/nosFond-img4.png"
-import image_nosFond_second1 from "./img/image-nosFond-second-1.png"
-import image_nosFond_second2 from "./img/image-nosFond-second-2.png"
-import MainLayout from '../../MainLayout/MainLayout'
-import HeaderBackground from '../../Components/HeaderBackground/HeaderBackground'
-
+import React from "react";
+import nosFond_img1 from "./img/nosFond_img1.png";
+import nosFond_img2 from "./img/nosFond-img2.png";
+import nosFond_img3 from "./img/nosFond-img3.png";
+import nosFond_img4 from "./img/nosFond-img4.png";
+import Carousel from "react-elastic-carousel";
+import MainLayout from "../../MainLayout/MainLayout";
+import HeaderBackground from "../../Components/HeaderBackground/HeaderBackground";
+import './NosFond.css'
 
 const NosFond = () => {
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 1200, itemsToShow: 3 },
+      ];
+      const Fondsimgs =[nosFond_img1,nosFond_img2,nosFond_img3,nosFond_img4]
   return (
     <>
-    <MainLayout>
-    <HeaderBackground />
-        <div className='nosFonds'>
-            <h1 className='big_title'>NOS FONDS</h1>
-            <div className='nosFond-container-1'>
-                <h2 className='title1'>1-NOS INVESTISSEURS</h2>
-                <p className='nosFond-parag-1'> STB SICAR dispose de fonds apportés par des investisseurs institutionnels de premier plan.</p>
-                <div className='image-nosFond-first'>
-                    <img className='image-nosFond-first-1' src={nosFond_img1} alt=""/>
-                    <img className='image-nosFond-first-2' src={nosFond_img2} alt=""/>
-                    <img className='image-nosFond-first-3'  src={nosFond_img3} alt="" />
-                    <img className='image-nosFond-first-4'  src={nosFond_img4} alt="" />
-                </div>
+      <MainLayout>
+        <HeaderBackground />
+        <div className="nosFonds">
+          <h1 className="page-title">NOS FONDS</h1>
+          <div className="nosFond-container-1">
+            <h3>1-NOS INVESTISSEURS</h3>
+            <p className="page-description">
+              {" "}
+              STB SICAR dispose de fonds apportés par des investisseurs
+              institutionnels de premier plan.
+            </p>
+            <div className="image-nosFond-first">
+            <Carousel
+                itemPadding={[10, 50]}
+                enableAutoPlay
+                autoPlaySpeed={1800}
+                outerSpacing={100}
+                breakPoints={breakPoints}
+                pagination={false}
+                showArrows={false}
+              >
+                  {Fondsimgs.map((img) => (
+                 <img className="Fonds__img" src={img} alt="img" />
+                ))}
+                  </Carousel>
+              
             </div>
-            <div className='nosFond-container-2'>    
-                <h2 className='title1'>2-OPTIMISEZ VOTRE FISCALITE</h2>
-                <p className='nosFond-parag-2'>Les investisseurs qui désirent optimiser leur fiscalité peuvent confier tout ou partie de leurs bénéfices imposables à la STB SICAR sous forme de fonds géré.</p>
-                <p className='nosFond-parag-2'> La règlementation en vigueur leur permet d’opter pour l’un des schémas suivants :</p>
-                <div className='image-nosFond-second'>
-                    <img className='image_nosFond_second1' src={image_nosFond_second1}></img>
-                    <img className='image_nosFond_second2' src={image_nosFond_second2}></img>
-                    {/* <img className='image-nosFond-second-3'></img> */}
+          </div>
+          <div className="nosFond-container-2">
+            <h3>2-OPTIMISEZ VOTRE FISCALITE</h3>
+            <p className="page-description">
+              Les investisseurs qui désirent optimiser leur fiscalité peuvent
+              confier tout ou partie de leurs bénéfices imposables à la STB
+              SICAR sous forme de fonds géré.
+            </p>
+            <p className="page-description">
+              {" "}
+              La règlementation en vigueur leur permet d’opter pour l’un des
+              schémas suivants :
+            </p>
+            <div className="image-nosFond-second">
+              <div className="fond__card__container">
+                <h4 className="home-main-subtitles">
+                  {" "}
+                  100% des revenus ou bénéfices imbosable sans minumum d'impot
+                </h4>
+                <div className="card__subcontainer">
+                  <p className="page-description">
+                    Deduction totale sur la base d'un engagement de la STB SICAR
+                    d'employer 75% de mentant déposés dans des ZDR ou aricole
+                  </p>
                 </div>
-
-
-                <div className='nosFond-delai'>
-                    <h3 className='nosFond-title-3'>Délai </h3>
-                    <p className='nosFond-parag-3'>Les investisseurs bénéficient de l’avantage fiscal l’année de libération du fonds.</p>
-                    <p className='nosFond-parag-3'>Néanmoins l’obligation d’utilisation des montants déposés est de 2 ans à partir de l’année de libération.</p>
+              </div>
+              <div className="fond__card__container">
+                <h4 className="home-main-subtitles">
+                  {" "}
+                  100% des revenus ou bénéfices imbosable sans minumum d'impot
+                </h4>
+                <div className="card__subcontainer">
+                  <p className="page-description">
+                    Deduction totale sur la base d'un engagement de la STB SICAR
+                    d'employer 75% de mentant déposés dans des ZDR ou aricole
+                  </p>
                 </div>
-                <div className='nosFond-blockage'>
-                    <h3 className='nosFond-title-3'>Blocage des fonds</h3>
-                    <p className='nosFond-parag'>Le fonds géré est bloqué auprès de la <strong>STB SICAR </strong> pendant cinq ans à partir du 1er janvier de l’année qui suit celle du dépôt des fonds.</p>
-                </div>
-                <div className='nosFond-remuniration'>
-                    <h3 className='nosFond-title-3'>Blocage des fonds</h3>
-                    <p className='nosFond-parag3'>Le bailleur de fonds bénéficiera également conformément à la convention Fonds géré :</p>
-                    <ul className='nosFond-remuniration-ul'>
-                        <li className='nosFond-remuniration-li'>-	De la rémunération des fonds à des conditions très avantageuses jusqu’à leur utilisation dans des projets ayant obtenu au préalable son accord de participation</li>
-                        <li className='nosFond-remuniration-li'>-	Du produit des intérêts des OCA/CCA souscrits</li>
-                        <li className='nosFond-remuniration-li'>-	Des revenus du portefeuille d’investissement</li>
-                        <li className='nosFond-remuniration-li'>-	Des dividendes distribués</li>
-                    </ul>
-                </div>
-                <div className='nosFond_autre_avantage'>
-                    <h3 className='nosFond-title-3'>Autre avantage</h3>
-                    <p className='nosFond-parag3'>Est déductible du bénéfice imposable, la plus-value de cession d’actions et des parts sociales réalisées. </p>
-                </div>
+              </div>
+              {/* <img
+                className="image_nosFond_second2"
+                src={image_nosFond_second2}
+              ></img> */}
             </div>
+
+            <div className="nosFond-delai">
+              <h3>Délai </h3>
+              <p className="page-description">
+                Les investisseurs bénéficient de l’avantage fiscal l’année de
+                libération du fonds.
+              </p>
+              <p className="page-description">
+                Néanmoins l’obligation d’utilisation des montants déposés est de
+                2 ans à partir de l’année de libération.
+              </p>
+            </div>
+            <div className="nosFond-blockage">
+              <h3>Blocage des fonds</h3>
+              <p className="page-description">
+                Le fonds géré est bloqué auprès de la{" "}
+                <strong>STB SICAR </strong> pendant cinq ans à partir du 1er
+                janvier de l’année qui suit celle du dépôt des fonds.
+              </p>
+            </div>
+            <div className="nosFond-remuniration">
+              <h3>Blocage des fonds</h3>
+              <p className="page-description">
+                Le bailleur de fonds bénéficiera également conformément à la
+                convention Fonds géré :
+              </p>
+              <div className="nosFond-remuniration-div">
+                <p className="page-description">
+                  - De la rémunération des fonds à des conditions très
+                  avantageuses jusqu’à leur utipsation dans des projets ayant
+                  obtenu au préalable son accord de participation
+                </p>
+                <p className="page-description">
+                  - Du produit des intérêts des OCA/CCA souscrits
+                </p>
+                <p className="page-description">
+                  - Des revenus du portefeuille d’investissement
+                </p>
+                <p className="page-description">- Des dividendes distribués</p>
+              </div>
+            </div>
+            <div className="nosFond_autre_avantage">
+              <h3>Autre avantage</h3>
+              <p className="page-description">
+                Est déductible du bénéfice imposable, la plus-value de cession
+                d’actions et des parts sociales réalisées.{" "}
+              </p>
+            </div>
+          </div>
         </div>
-        </MainLayout>
+      </MainLayout>
     </>
-  )
-}
+  );
+};
 
-export default NosFond
+export default NosFond;
